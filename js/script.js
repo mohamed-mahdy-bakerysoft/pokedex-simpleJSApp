92f10b7d56af183f500b2294ed4d71da8857badf
Amruta <amruta.patil387@gmail.com>
2024-11-04 12:38:58 +0100

    updated using IIFE
    updated for loop with forEach and IIFE

--- js/script.js
+++ js/script.js
@@ -2,42 +2,6 @@
 
 let favoriteFood = 'Chicken Biryani';
 document.write(favoriteFood);*/
-
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        types: ['grass', 'poison']
-
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass', 'poison']
-
-    },
-    {
-        name:'Charmander',
-        height: 0.6 ,
-        types:['fire']
-    },
-    {
-        name:'Butterfree',
-        height: 1.1 ,
-        types:['bug' , 'flying']
-    },
-    {
-        name: 'Nidoking',
-        height: 1.4,
-        types: ['ground', 'poison']
-    }
-];
-
 /*let message = "";
 
 //for loop to display list on the page
@@ -54,8 +18,33 @@ for(let i= 0; i < pokemonList.length; i++){
     );
 } */
 
+
+//Exercise 1.5 solution part 2 IIFE
+let pokemonRepository = (function() {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+        { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+        { name: 'Venusaur', height: 2, types: ['grass', 'poison'] },
+        { name:'Charmander', height: 0.6 , types:['fire'] },
+        { name:'Butterfree', height: 1.1 , types:['bug' , 'flying'] },
+        { name: 'Nidoking', height: 1.4, types: ['ground', 'poison'] },
+    ];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+let pokemons = pokemonRepository.getAll();
+
 //Exercise 1.5 solution part 1
-pokemonList.forEach(pokemon => {
+pokemons.forEach(pokemon => {
     if(pokemon.height >= 1){
         document.write('Wow that is big <br><br>');
     }
